9aa5b4c7a21bc0dcfe5886b85f6802e4718ba010
Anton Clever <antvcl@gmail.com>
2023-01-17 19:42:54 -0700

    Quick fix

--- controllers/auth/updateAvatar.js
+++ controllers/auth/updateAvatar.js
@@ -13,7 +13,7 @@ const updateAvatar = async (req, res) => {
   const resultUpload = path.join(avatarsDir, filename);
 
   const image = await Jimp.read(tempUpload);
-  image.resize(250, 250).write(resultUpload);
+  image.resize(250, 250).writeAsync(tempUpload);
 
   await fs.rename(tempUpload, resultUpload);
   const avatarURL = path.join("avatars", filename);
